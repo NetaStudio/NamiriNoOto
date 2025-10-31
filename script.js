2f1459918ca78542aa1603bb82fed7f79661bba5
NetaStudio <netakoubou@gmail.com>
2025-10-31 14:21:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -22,51 +22,219 @@ const VOICE_DATA =
             { text: "聞こえてるかなぁ", file: "聞こえてるかなぁ.mp3" },
             { text: "聞こえてそうだね", file: "聞こえてそうだね.mp3" },
             { text: "ただいま", file: "ただいま.mp3" },
+            { text: "またね～", file: "またね～.mp3" },
             { text: "またね", file: "またね.mp3" },
-            { text: "またね！", file: "またね！.mp3" },
-            { text: "ばいばい", file: "ばいばい.mp3" },
-            { text: "行ってきまーす", file: "行ってきまーす.mp3" },
-            { text: "こんにちわ", file: "こんにちわ.mp3" },
-            { text: "こんみり～", file: "こんみり～.mp3" },
+            { text: "また！", file: "また！.mp3" },
+            { text: "おやすみ～", file: "おやすみ～.mp3" },
+            { text: "ばいば～い", file: "ばいば～い.mp3" },
+            { text: "よ～しくお願いします", file: "よ～しくお願いします.mp3" },
+            { text: "君のハート", file: "君のハート.mp3" },
+            { text: "びしょぬれ注意だよ？", file: "びしょぬれ注意だよ？.mp3" },
+            { text: "誕生日は1月26日", file: "誕生日は1月26日.mp3" },
+            { text: "身長は153cmです", file: "身長は153cmです.mp3" },
+            { text: "大きいケーキに飛び込みたい", file: "大きいケーキに飛び込みたい.mp3" },
         ]
     },
     {
-        id: "category-reaction",
-        name: "反応",
-        en_name: "Reaction",
-        folder: "02_reaction",
+        id: "category-positive",
+        name: "肯定",
+        en_name: "Positive",
+        folder: "02_positive",
         voices:
         [
-            { text: "あ！", file: "あ！.mp3" },
-            { text: "いぇい！", file: "いぇい！.mp3" },
-            { text: "えー", file: "えー.mp3" },
-            { text: "えへへ", file: "えへへ.mp3" },
-            { text: "おぉ～", file: "おぉ～.mp3" },
-            { text: "そうなんだ", file: "そうなんだ.mp3" },
-            { text: "だめだ", file: "だめだ.mp3" },
-            { text: "やったー！", file: "やったー！.mp3" },
-            { text: "わぁ～", file: "わぁ～.mp3" },
-            { text: "ん～", file: "ん～.mp3" },
+            { text: "はーい", file: "はーい.mp3" },
+            { text: "はい", file: "はい.mp3" },
+            { text: "ん", file: "ん.mp3" },
+            { text: "へい", file: "へい.mp3" },
+            { text: "いいよ特別ね", file: "いいよ特別ね.mp3" },
+            { text: "OK", file: "OK.mp3" },
+            { text: "OKです", file: "OKです.mp3" },
+            { text: "ぜひぜひ", file: "ぜひぜひ.mp3" },
+            { text: "歓迎です", file: "歓迎です.mp3" },
+            { text: "ありがとー", file: "ありがとー.mp3" },
+            { text: "ありがたいです", file: "ありがたいです.mp3" },
+            { text: "本当にありがとうございます", file: "本当にありがとうございます.mp3" },
+            { text: "Thank you", file: "Thank you.mp3" },
         ]
     },
     {
-        id: "category-emotion",
-        name: "感情",
-        en_name: "Emotion",
-        folder: "03_emotion",
+        id: "category-negative",
+        name: "否定",
+        en_name: "Negative",
+        folder: "03_negative",
         voices:
         [
-            { text: "うそ！", file: "うそ！.mp3" },
-            { text: "すごい！", file: "すごい！.mp3" },
-            { text: "どーしよー", file: "どーしよー.mp3" },
-            { text: "なるほど", file: "なるほど.mp3" },
-            { text: "ひゃっ", file: "ひゃっ.mp3" },
-            { text: "マジで！？", file: "マジで！？.mp3" },
-            { text: "やったぜ", file: "やったぜ.mp3" },
-            { text: "ヤバイ", file: "ヤバイ.mp3" },
+            { text: "ダメ", file: "ダメ.mp3" },
+            { text: "ダメです", file: "ダメです.mp3" },
+            { text: "だ～め", file: "だ～め.mp3" },
+            { text: "やーだね", file: "やーだね.mp3" },
+            { text: "やんないよ", file: "やんないよ.mp3" },
+            { text: "してな～い", file: "してな～い.mp3" },
+            { text: "あ～げない", file: "あ～げない.mp3" },
+            { text: "気持ち悪いんだから", file: "気持ち悪いんだから.mp3" },
+            { text: "きめー", file: "きめー.mp3" },
+            { text: "なに考えてんの全く", file: "なに考えてんの全く.mp3" },
+            { text: "なに期待してんのよ", file: "なに期待してんのよ.mp3" },
+            { text: "何してんだ！", file: "何してんだ！.mp3" },
+            { text: "許しません", file: "許しません.mp3" },
+            { text: "そんなバナナー！", file: "そんなバナナー！.mp3" },
+            { text: "もうえぇでしょ", file: "もうえぇでしょ.mp3" },
+        ]
+    },
+    {
+        id: "category-filler",
+        name: "繋ぎ",
+        en_name: "Filler",
+        folder: "04_filler",
+        voices:
+        [
+            { text: "あの→", file: "あの→.mp3" },
+            { text: "あの↓", file: "あの↓.mp3" },
+            { text: "あ～", file: "あ～.mp3" },
+            { text: "え～と", file: "え～と.mp3" },
+            { text: "てことで", file: "てことで.mp3" },
+            { text: "てことでぇ", file: "てことでぇ.mp3" },
+            { text: "もう", file: "もう.mp3" },
+            { text: "ん～？", file: "ん～？.mp3" },
+            { text: "んしょ～", file: "んしょ～.mp3" },
+            { text: "いや、まぁ本当", file: "いや、まぁ本当.mp3" },
+        ]
+    },
+    {
+        id: "category-joy",
+        name: "喜び",
+        en_name: "Joy",
+        folder: "05_joy",
+        voices:
+        [
+            { text: "いえーい", file: "いえーい.mp3" },
+            { text: "よしよしよし", file: "よしよしよし.mp3" },
+            { text: "やったーはははは", file: "やったーはははは.mp3" },
+            { text: "嬉しいな～ななな～な", file: "嬉しいな～ななな～な.mp3" },
+            { text: "ばーーーーか", file: "ばーーーーか.mp3" },
+            { text: "まじっすか", file: "まじっすか.mp3" },
+            { text: "満足かな", file: "満足かな.mp3" },
+        ]
+    },
+    {
+        id: "category-anger",
+        name: "怒り",
+        en_name: "Anger",
+        folder: "06_anger",
+        voices:
+        [
+            { text: "ふん", file: "ふん.mp3" },
+            { text: "まったく", file: "まったく.mp3" },
+            { text: "黙れ", file: "黙れ.mp3" },
+            { text: "黙りなさい", file: "黙りなさい.mp3" },
+            { text: "鎮まれ", file: "鎮まれ.mp3" },
+            { text: "静粛に", file: "静粛に.mp3" },
+            { text: "あ静かに", file: "あ静かに.mp3" },
+            { text: "静かにしなさい", file: "静かにしなさい.mp3" },
+            { text: "いい加減にしろ", file: "いい加減にしろ.mp3" },
+            { text: "クソ野郎", file: "クソ野郎.mp3" },
+            { text: "舐めんじゃねーぞ", file: "舐めんじゃねーぞ.mp3" },
+            { text: "ふざけんな", file: "ふざけんな.mp3" },
+            { text: "知らないんだから", file: "知らないんだから.mp3" },
+            { text: "不機嫌になってきたなぁ", file: "不機嫌になってきたなぁ.mp3" },
+            { text: "そういうこと考えてんだ", file: "そういうこと考えてんだ.mp3" },
+        ]
+    },
+    {
+        id: "category-sad",
+        name: "哀しい",
+        en_name: "Sad",
+        folder: "07_sad",
+        voices:
+        [
+            { text: "悲しいよ", file: "悲しいよ.mp3" },
+            { text: "悲しすぎるよ", file: "悲しすぎるよ.mp3" },
+            { text: "もうバカ！", file: "もうバカ！.mp3" },
+            { text: "この有様でした", file: "この有様でした.mp3" },
+            { text: "うぇ～へへへ", file: "うぇ～へへへ.mp3" },
+            { text: "ク～ッソ", file: "ク～ッソ.mp3" },
+            { text: "ハァ", file: "ハァ.mp3" },
+            { text: "不安だな", file: "不安だな.mp3" },
+            { text: "すまん", file: "すまん.mp3" },
+            { text: "気付いてませんでした", file: "気付いてませんでした.mp3" },
+        ]
+    },
+    {
+        id: "category-happy",
+        name: "楽しい",
+        en_name: "Happy",
+        folder: "08_happy",
+        voices:
+        [
+            { text: "楽しみだなぁ", file: "楽しみだなぁ.mp3" },
+            { text: "わくわくすっぞ", file: "わくわくすっぞ.mp3" },
+            { text: "いや～はははは", file: "いや～はははは.mp3" },
+            { text: "かはははは", file: "かはははは.mp3" },
+            { text: "ふふはは", file: "ふふはは.mp3" },
+            { text: "んふふふふ", file: "んふふふふ.mp3" },
+            { text: "楽しい航海にしてやろうじゃん", file: "楽しい航海にしてやろうじゃん.mp3" },
+        ]
+    },
+    {
+        id: "category-inciting",
+        name: "煽り",
+        en_name: "Inciting",
+        folder: "11_inciting",
+        voices:
+        [
+            { text: "へ～", file: "へ～.mp3" },
+            { text: "お～い", file: "お～い.mp3" },
+            { text: "そんなこともできないの？", file: "そんなこともできないの？.mp3" },
+            { text: "ざーーーこ", file: "ざーーーこ.mp3" },
+            { text: "残念でした", file: "残念でした.mp3" },
+            { text: "ごめ～んね？", file: "ごめ～んね？.mp3" },
+            { text: "ばーか", file: "ばーか.mp3" },
+            { text: "おい見たか", file: "おい見たか.mp3" },
+            { text: "まぁまぁまぁまぁ", file: "まぁまぁまぁまぁ.mp3" },
+            { text: "良い子だね？", file: "良い子だね？.mp3" },
+            { text: "キャー怖～いw", file: "キャー怖～いw.mp3" },
+        ]
+    },
+    {
+        id: "category-onomatopoeia",
+        name: "擬音",
+        en_name: "Onomatopoeia",
+        folder: "12_onomatopoeia",
+        voices:
+        [
+            { text: "まぎまぎ", file: "まぎまぎ.mp3" },
+            { text: "むぎむぎ", file: "むぎむぎ.mp3" },
+            { text: "わせわせ", file: "わせわせ.mp3" },
+            { text: "グルグル", file: "グルグル.mp3" },
+            { text: "バクバク", file: "バクバク.mp3" },
+            { text: "ブルブル", file: "ブルブル.mp3" },
+            { text: "ドン", file: "ドン.mp3" },
+            { text: "どかーん", file: "どかーん.mp3" },
+            { text: "えっほえっほ", file: "えっほえっほ.mp3" },
+            { text: "じーーーー", file: "じーーーー.mp3" },
+            { text: "むにゅ", file: "むにゅ.mp3" },
+        ]
+    },
+    {
+        id: "category-other",
+        name: "その他",
+        en_name: "Other",
+        folder: "13_other",
+        voices:
+        [
+            { text: "起きろ～", file: "起きろ～.mp3" },
+            { text: "起きなさ～い", file: "起きなさ～い.mp3" },
+            { text: "ちょと待ってね", file: "ちょと待ってね.mp3" },
+            { text: "一緒にいこ！", file: "一緒にいこ！.mp3" },
+            { text: "戻らなくちゃ", file: "戻らなくちゃ.mp3" },
+            { text: "美味しくな～れ", file: "美味しくな～れ.mp3" },
+            { text: "はい、あ～ん？", file: "はい、あ～ん？.mp3" },
+            { text: "可愛いでしょ？", file: "可愛いでしょ？.mp3" },
+            { text: "今のなんか語弊あったかなぁ", file: "今のなんか語弊あったかなぁ.mp3" },
+            { text: "何見てんのセクハラ", file: "何見てんのセクハラ.mp3" },
+            { text: "忘れろビーム", file: "忘れろビーム.mp3" },
         ]
     }
-    // ... 必要に応じてカテゴリを追加 ...
 ];
 
 const FAVORITES_KEY = 'shionami_favorites';
@@ -564,3 +732,4 @@ document.addEventListener('DOMContentLoaded', () => {
     messageContainer.id = 'message-container';
     document.body.appendChild(messageContainer);
 });
+
